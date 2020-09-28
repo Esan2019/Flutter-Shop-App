ae64ed97edc2c5b1d43187ca006875e3347c1c59
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-09-28 07:11:16 -0300

    Routes handler

---
+++ lib/routes_handler.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import './routes/home.dart';
+
+const homeRoute = '/';
+
+class RoutesHandler {
+  static Route<dynamic> _navigate(Widget destinationPage) {
+    return MaterialPageRoute(builder: (_) => destinationPage);
+  }
+
+  static Route<dynamic> handleRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case homeRoute:
+        _navigate(Home());
+    }
+  }
+}
