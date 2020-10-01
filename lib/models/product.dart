d7838cf5761e9b07418f2a2a7541891d8b7cdd87
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-01 11:51:40 -0300

    Product provider

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,11 +1,16 @@
 import 'package:flutter/foundation.dart';
 
-class Product {
-  @required final int id;
-  @required final String title;
-  @required final String description;
-  @required final double price;
-  @required final String imageUrl;
+class Product with ChangeNotifier {
+  @required
+  final int id;
+  @required
+  final String title;
+  @required
+  final String description;
+  @required
+  final double price;
+  @required
+  final String imageUrl;
   bool isFavorite;
 
   Product(
@@ -15,4 +20,9 @@ class Product {
       this.price,
       this.imageUrl,
       this.isFavorite = false});
+
+  void toggleFavoriteStatus() {
+    isFavorite = !isFavorite;
+    notifyListeners();
+  }
 }
