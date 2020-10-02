1e050f821e745e59d1624a161725e5c64acead26
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-02 17:01:45 -0300

    Gestures are now handled by the ProductCard

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -2,10 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
 
-import '../models/product.dart';
 import '../widgets/product_card.dart';
 import '../widgets/drawer.dart';
-import '../routes_handler.dart';
 import '../providers/products.dart';
 
 class Home extends StatelessWidget {
@@ -26,63 +24,14 @@ class Home extends StatelessWidget {
           )
         ],
       ),
-      body: Builder(
-        builder: (builderContext) {
-          final currentScaffoldState = Scaffold.of(builderContext);
-          return ListView.builder(
-            padding: const EdgeInsets.only(top: 0),
-            itemBuilder: (_, index) {
-              return Dismissible(
-                key: ValueKey(products[index].id),
-                // TODO: implement confirmDismiss function
-                confirmDismiss: (_) async {
-                  _showSnackBar(currentScaffoldState,
-                      'Salvo na sacolinha: ${products[index].title}');
-                  return false;
-                },
-                direction: DismissDirection.startToEnd,
-                background: Container(
-                  padding: const EdgeInsets.only(left: 10),
-                  color: Theme.of(context).accentColor,
-                  alignment: Alignment.centerLeft,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      Icon(Icons.shopping_bag, size: 40),
-                      Text('Salvar na sacolinha',
-                          style: TextStyle(fontWeight: FontWeight.w500))
-                    ],
-                  ),
-                ),
-                child: GestureDetector(
-                  child: ChangeNotifierProvider.value(
-                      child: ProductCard(), value: products[index]),
-                  onTap: () {
-                    _navigateToOverviewScreen(context, products[index]);
-                  },
-                  onDoubleTap: () => products[index].toggleFavoriteStatus(),
-                ),
-              );
-            },
-            itemCount: products.length,
-            physics: const BouncingScrollPhysics(),
-          );
+      body: ListView.builder(
+        padding: const EdgeInsets.only(top: 0),
+        itemCount: products.length,
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (_, index) {
+          return ProductCard(products.elementAt(index));
         },
       ),
     );
-  }
-
-  void _showSnackBar(ScaffoldState scaffold, String content) {
-    scaffold.hideCurrentSnackBar();
-    scaffold.showSnackBar(
-      SnackBar(content: Text(content), duration: Duration(seconds: 2)),
-    );
-  }
-
-  void _navigateToOverviewScreen(
-      BuildContext context, Product selectedProduct) {
-    Navigator.of(context)
-        .pushNamed(productOverviewRoute, arguments: selectedProduct);
   }
 }
