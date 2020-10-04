3240f98a90588822c2c2f76e65998ca2832afb1f
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-04 19:37:32 -0300

    Product model encapsulation

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,27 +1,45 @@
 import 'package:flutter/foundation.dart';
 
-class Product{
+class Product {
   @required
-  final int id;
+  final int _id;
   @required
-  final String title;
+  final String _title;
   @required
-  final String description;
+  final String _description;
   @required
-  final double price;
+  final double _price;
   @required
-  final String imageUrl;
-  bool isFavorite;
+  final String _imageUrl;
+  bool _isFavorite;
 
-  Product(
-      {this.id,
-      this.title,
-      this.description,
-      this.price,
-      this.imageUrl,
-      this.isFavorite = false});
+  Product({
+    @required int id,
+    @required String title,
+    @required String description,
+    double price,
+    @required String imageUrl,
+    bool isFavorite,
+  })  : _id = id,
+        _title = title,
+        _description = description,
+        _price = price ?? 0.0,
+        _imageUrl = imageUrl,
+        _isFavorite = isFavorite ?? false;
+
+  int get id => _id;
+
+  String get title => _title;
+
+  String get description => _description;
+
+  double get price => _price;
+
+  String get imageUrl => _imageUrl;
+
+  bool get isFavorite => _isFavorite;
 
   void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
+    _isFavorite = !_isFavorite;
   }
 }
