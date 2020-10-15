5249ce66a44354d09570d7580ebc499e5d54f1ac
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-15 01:06:59 -0300

    Helper class for getting device screen proportions

---
+++ lib/size_config.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/widgets.dart';
+
+class SizeConfig {
+  static MediaQueryData mediaQuery;
+  static double screenHeight, screenWidth;
+  static Orientation orientation;
+  static double textScaleFactor;
+
+  static bool get isInLandscape => orientation == Orientation.landscape;
+
+  static bool get isInPortrait => orientation == Orientation.portrait;
+
+  static double getHeightPercentage(double percentage) =>
+      screenHeight * (percentage / 100);
+
+  static double getWidthPercentage(double percentage) =>
+      screenWidth * (percentage / 100);
+
+  static void init(BuildContext context) {
+    mediaQuery = MediaQuery.of(context);
+    screenHeight = mediaQuery.size.height;
+    screenWidth = mediaQuery.size.width;
+    orientation = mediaQuery.orientation;
+  }
+
+  static SizedBox getSpacing({double vertical = 0, double horizontal = 0}) {
+    return SizedBox(
+      height: getHeightPercentage(vertical),
+      width: getWidthPercentage(horizontal),
+    );
+  }
+}
