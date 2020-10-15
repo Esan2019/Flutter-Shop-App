1386965da37a6a3d0318f2ae68d58ddd004e2807
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-14 21:14:29 -0300

    Order card

---
+++ lib/widgets/order_card.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import 'package:intl/intl.dart';
+
+import '../models/order.dart';
+
+class ExpandableOrderCard extends StatelessWidget {
+  final Order order;
+  ExpandableOrderCard(this.order);
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionTile(
+      expandedCrossAxisAlignment: CrossAxisAlignment.start,
+      childrenPadding: EdgeInsets.all(18),
+      key: ValueKey(order.id),
+      title: Text(
+        DateFormat('dd/MM/yyyy - HH:mm').format(order.moment),
+        style: TextStyle(fontWeight: FontWeight.w600),
+      ),
+      subtitle: Text(
+        '${order.totalItemsQuantity} itens, totalizando R\$${order.totalValue.toStringAsFixed(2)}',
+        style: TextStyle(fontWeight: FontWeight.w500),
+      ),
+      children: order.items.map((cartItem) {
+        return ListTile(
+          onLongPress: null,
+          title: Text(
+            cartItem.product.title,
+            style: TextStyle(fontSize: 14),
+          ),
+          leading: Text('${cartItem.quantity}x'),
+          trailing: Text('R\$${cartItem.product.price.toStringAsFixed(2)}'),
+        );
+      }).toList(),
+    );
+  }
+}
