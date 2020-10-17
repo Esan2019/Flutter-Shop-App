4c6e18d98eca4bc8fa2c36dbb947bca45914892b
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-16 22:02:17 -0300

    Refactored product card and gestures

---
+++ lib/widgets/product_card/product_card.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+import '../../size_config.dart';
+import '../../models/product.dart';
+
+const productTitleStyle = TextStyle(
+  color: const Color(0xFFFFFFFF),
+  fontWeight: FontWeight.w500,
+  shadows: [
+    Shadow(
+      color: const Color(0xFF000000),
+      blurRadius: 8,
+    ),
+    Shadow(
+      color: const Color(0xFF000000),
+      blurRadius: 8,
+    ),
+  ],
+);
+
+const productPriceStyle = TextStyle(
+  color: const Color(0xFFFFFFFF),
+  fontWeight: FontWeight.bold,
+  shadows: [
+    Shadow(
+      color: const Color(0xFF000000),
+      blurRadius: 8,
+    ),
+  ],
+);
+
+class ProductCard extends StatelessWidget {
+  final Product product;
+  final List<Widget> icons;
+  ProductCard(this.product, {this.icons});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 12,
+      margin: const EdgeInsets.all(10),
+      child: Stack(
+        children: [
+          Container(
+            width: double.infinity,
+            height: SizeConfig.getHeightPercentage(50),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            left: 0,
+            right: 0,
+            child: Container(
+              padding: const EdgeInsets.all(8),
+              decoration: BoxDecoration(
+                color: const Color(0xFFf5d9ff).withOpacity(0.65),
+                boxShadow: [
+                  BoxShadow(
+                    color: const Color(0xFF000000).withOpacity(0.3),
+                  )
+                ],
+              ),
+              child: Column(
+                children: [
+                  Container(
+                    height: SizeConfig.getHeightPercentage(3.5),
+                    alignment: Alignment.centerLeft,
+                    child: FittedBox(
+                      child: Text(product.title, style: productTitleStyle),
+                    ),
+                  ),
+                  Container(
+                    height: SizeConfig.getHeightPercentage(3.5),
+                    alignment: Alignment.centerLeft,
+                    child: FittedBox(
+                      child: Row(
+                        children: [
+                          ...?icons,
+                          Text(
+                            'R\$ ${product.price}',
+                            style: productPriceStyle,
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ lib/widgets/product_card/product_card_gestures.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import 'product_card.dart';
+
+class ProductCardGestures extends StatelessWidget {
+  final ValueKey<int> key;
+
+  final Function onTap;
+  final Function onDoubleTap;
+
+  final Function onRightSwipe;
+  final Widget rightSwipeBackground;
+
+  final Function onLeftSwipe;
+  final Widget leftSwipeBackground;
+
+  final ProductCard child;
+
+  ProductCardGestures({
+    @required this.key,
+    @required this.onTap,
+    this.onDoubleTap,
+    @required this.onRightSwipe,
+    @required this.rightSwipeBackground,
+    this.onLeftSwipe,
+    this.leftSwipeBackground,
+    @required this.child,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Dismissible(
+      key: ValueKey(key),
+      direction: onLeftSwipe != null
+          ? DismissDirection.horizontal
+          : DismissDirection.startToEnd,
+      confirmDismiss: (direction) async {
+        switch (direction) {
+          case DismissDirection.startToEnd:
+            onRightSwipe();
+            break;
+          case DismissDirection.endToStart:
+            onLeftSwipe();
+            break;
+          default:
+            return false;
+        }
+        return false;
+      },
+      background: rightSwipeBackground,
+      secondaryBackground: leftSwipeBackground,
+      child: GestureDetector(
+        child: child,
+        onTap: onTap,
+        onDoubleTap: onDoubleTap,
+      ),
+    );
+  }
+}
