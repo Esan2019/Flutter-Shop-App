3d558267ed59fe341131cbfc8b675542cc1f89e3
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-17 01:36:29 -0300

    Gestures and action in user products

--- lib/routes/user_products.dart
+++ lib/routes/user_products.dart
@@ -5,6 +5,8 @@ import 'package:provider/provider.dart';
 import '../providers/products.dart';
 import '../widgets/drawer.dart';
 import '../widgets/product_card/product_card.dart';
+import '../widgets/product_card/product_card_gestures.dart';
+import '../widgets/product_card/gesture_background.dart';
 
 class UserProducts extends StatelessWidget {
   @override
@@ -14,12 +16,41 @@ class UserProducts extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Gerenciar produtos'),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.add),
+            // TODO: implement onPressed function
+            onPressed: () {},
+          )
+        ],
       ),
       drawer: MainDrawer(),
       body: ListView.builder(
         itemCount: products.length,
         physics: BouncingScrollPhysics(),
-        itemBuilder: (_, index) => ProductCard(products.elementAt(index)),
+        itemBuilder: (_, index) => ProductCardGestures(
+          key: ValueKey<int>(products.elementAt(index).id),
+          child: ProductCard(products.elementAt(index)),
+          onTap: () {},
+          // TODO: implement onRightSwipe function
+          onRightSwipe: () {},
+          rightSwipeBackground: const GestureBackground(
+            icon: Icons.edit,
+            label: 'Editar item',
+            color: const Color(0xFF000000),
+            backgroundColor: const Color(0xFFF2804E),
+            alignment: Alignment.centerLeft,
+          ),
+          // TODO: implement onLeftSwipe function
+          onLeftSwipe: () {},
+          leftSwipeBackground: const GestureBackground(
+            icon: Icons.delete,
+            label: 'Deletar item',
+            color: const Color(0xFFFFFFFF),
+            backgroundColor: const Color(0xFFFF0000),
+            alignment: Alignment.centerRight,
+          ),
+        ),
       ),
     );
   }
