fa8ccd7da1b73e74982a27348467631c88171690
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-10-18 05:09:32 -0300

    Swipe to delete product

--- lib/routes/user_products.dart
+++ lib/routes/user_products.dart
@@ -11,11 +11,12 @@ import '../widgets/product_card/gesture_background.dart';
 class UserProducts extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final products = Provider.of<Products>(context).products;
+    final productsProvider = Provider.of<Products>(context);
+    final products = productsProvider.products;
 
     return Scaffold(
       appBar: AppBar(
-        title: Text('Gerenciar produtos'),
+        title: const Text('Gerenciar produtos'),
         actions: [
           IconButton(
             icon: Icon(Icons.add),
@@ -27,30 +28,67 @@ class UserProducts extends StatelessWidget {
       drawer: MainDrawer(),
       body: ListView.builder(
         itemCount: products.length,
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (_, index) => ProductCardGestures(
-          key: ValueKey<int>(products.elementAt(index).id),
-          child: ProductCard(products.elementAt(index)),
-          onTap: () {},
-          // TODO: implement onRightSwipe function
-          onRightSwipe: () {},
-          rightSwipeBackground: const GestureBackground(
-            icon: Icons.edit,
-            label: 'Editar item',
-            color: const Color(0xFF000000),
-            backgroundColor: const Color(0xFFF2804E),
-            alignment: Alignment.centerLeft,
-          ),
-          // TODO: implement onLeftSwipe function
-          onLeftSwipe: () {},
-          leftSwipeBackground: const GestureBackground(
-            icon: Icons.delete,
-            label: 'Deletar item',
-            color: const Color(0xFFFFFFFF),
-            backgroundColor: const Color(0xFFFF0000),
-            alignment: Alignment.centerRight,
-          ),
-        ),
+        physics: const BouncingScrollPhysics(),
+        itemBuilder: (_, index) {
+          final product = products.elementAt(index);
+
+          return ProductCardGestures(
+            key: ValueKey<int>(product.id),
+            child: ProductCard(product),
+            onTap: () {},
+            // TODO: implement onRightSwipe function
+            onRightSwipe: () {},
+            rightSwipeBackground: const GestureBackground(
+              icon: Icons.edit,
+              label: 'Editar item',
+              color: const Color(0xFF000000),
+              backgroundColor: const Color(0xFFF2804E),
+              alignment: Alignment.centerLeft,
+            ),
+            onLeftSwipe: () {
+              showDialog(
+                context: context,
+                builder: (ctx) {
+                  return AlertDialog(
+                    title: Text('Confirmar deleção'),
+                    content: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        const Text(
+                          'Você está a um passo de deletar o seguinte produto:',
+                        ),
+                        const SizedBox(height: 8,),
+                        Text('${product.title}.', style: TextStyle(fontWeight: FontWeight.w500,)),
+                        const SizedBox(height: 8),
+                        const Text('Tem certeza que deseja prosseguir? Esta ação é irreversível!'),
+                      ],
+                    ),
+                    actions: [
+                      FlatButton(
+                        child: const Text('NÃO, HOUVE UM ENGANO'),
+                        onPressed: () => Navigator.of(ctx).pop(),
+                      ),
+                      FlatButton(
+                        child: Text('SIM, QUERO DELETAR', style: TextStyle(color: Theme.of(context).errorColor)),
+                        onPressed: () {
+                          productsProvider.deleteProduct(product);
+                          Navigator.of(ctx).pop();
+                        },
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            leftSwipeBackground: const GestureBackground(
+              icon: Icons.delete,
+              label: 'Deletar item',
+              color: const Color(0xFFFFFFFF),
+              backgroundColor: const Color(0xFFFF0000),
+              alignment: Alignment.centerRight,
+            ),
+          );
+        },
       ),
     );
   }
