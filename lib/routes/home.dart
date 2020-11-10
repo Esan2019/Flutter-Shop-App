3775dd8e79efeef3afc0a7cb53aca2f850c9a9e1
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-11-10 19:48:11 -0300

    Fetching products in home page

--- lib/routes/home.dart
+++ lib/routes/home.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import 'package:lottie/lottie.dart';
 
 import '../size_config.dart';
 import '../widgets/product_card/product_card.dart';
@@ -17,95 +18,122 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
-    final productsProvider = Provider.of<Products>(context);
-    final products = productsProvider.products;
+
+    final appBar = AppBar(
+      title: Text('Shop App'),
+      actions: [
+        Consumer<Cart>(
+          builder: (_, cart, child) => Badge(
+            child: child,
+            value: cart.totalItemsCount,
+          ),
+          child: FlatButton(
+            child: Row(
+              children: [
+                Icon(Icons.shopping_bag),
+                Text('Sacolinha'),
+              ],
+            ),
+            onPressed: () => Navigator.pushNamed(context, cartItemsRoute),
+          ),
+        ),
+      ],
+    );
 
     return Scaffold(
       drawer: MainDrawer(),
-      appBar: AppBar(
-        title: Text('Shop App'),
-        actions: [
-          Consumer<Cart>(
-            builder: (_, cart, child) => Badge(
-              child: child,
-              value: cart.totalItemsCount,
-            ),
-            child: FlatButton(
-              child: Row(
-                children: [
-                  Icon(Icons.shopping_bag),
-                  Text('Sacolinha'),
-                ],
-              ),
-              onPressed: () => Navigator.pushNamed(context, cartItemsRoute),
-            ),
-          ),
-        ],
-      ),
-      body: !productsProvider.hasAtLeastOneProduct
-          ? NoProductsWarning()
-          : ListView.builder(
-              padding: const EdgeInsets.only(top: 0),
-              itemCount: products.length,
-              physics: const BouncingScrollPhysics(),
-              itemBuilder: (_, index) {
-                final product = products.elementAt(index);
+      appBar: appBar,
+      body: FutureBuilder(
+        future: Provider.of<Products>(context, listen: false)
+            .fetchProductsFromDatabase(),
+        builder: (ctx, snapshot) {
+          if (snapshot.connectionState != ConnectionState.done) {
+            return Center(
+                child: Lottie.asset('assets/animations/bouncy-balls.json'));
+          } else {
+            if (snapshot.hasError) {
+              return Center(
+                child: Text(snapshot.error),
+              );
+            }
 
-                return Consumer2<Products, Cart>(
-                  builder: (ctx, products, cart, child) {
-                    final isInCart = cart.contains(product);
-                    final isFavorite = product.isFavorite;
-                    final removeItem = cart.removeProduct;
-                    final addItem = cart.addProduct;
+            return Consumer<Products>(
+              builder: (_, productsProvider, child) {
+                final products = productsProvider.products;
 
-                    return ProductCardGestures(
-                      key: ValueKey<String>(product.id),
-                      child: ProductCard(
-                        product,
-                        icons: _getIcons(
-                          isFavorite: isFavorite,
-                          isInCart: isInCart,
-                        ),
-                      ),
-                      onTap: () => Navigator.of(context).pushNamed(
-                        productOverviewRoute,
-                        arguments: product,
-                      ),
-                      onDoubleTap: () => products.toggleFavoriteStatus(product),
-                      onRightSwipe: () {
-                        if (isInCart) {
-                          removeItem(product);
-                          _showSnackbar(ctx, 'Removido da sacolinha!',
-                              () => addItem(product));
-                        } else {
-                          addItem(product);
-                          _showSnackbar(
-                            ctx,
-                            'Salvo na sacolinha!',
-                            () => removeItem(product),
-                          );
-                        }
-                      },
-                      rightSwipeBackground: isInCart
-                          ? GestureBackground(
-                              icon: Icons.shopping_bag,
-                              label: 'Remover da sacolinha',
-                              color: const Color(0xFFF5C6BC),
-                              backgroundColor: const Color(0xFFF2804E),
-                              alignment: Alignment.centerLeft,
-                            )
-                          : GestureBackground(
-                              icon: Icons.shopping_bag,
-                              label: 'Salvar na sacolinha',
-                              color: const Color(0xFFF5BCE4),
-                              backgroundColor: Theme.of(context).accentColor,
-                              alignment: Alignment.centerLeft,
+                if (!productsProvider.hasAtLeastOneProduct) {
+                  return NoProductsWarning();
+                } else {
+                  return ListView.builder(
+                    padding: const EdgeInsets.only(top: 0),
+                    itemCount: products.length,
+                    physics: const BouncingScrollPhysics(),
+                    itemBuilder: (_, index) {
+                      final product = products.elementAt(index);
+
+                      return Consumer2<Products, Cart>(
+                        builder: (ctx, products, cart, child) {
+                          final isInCart = cart.contains(product);
+                          final isFavorite = product.isFavorite;
+                          final removeItem = cart.removeProduct;
+                          final addItem = cart.addProduct;
+
+                          return ProductCardGestures(
+                            key: ValueKey<String>(product.id),
+                            child: ProductCard(
+                              product,
+                              icons: _getIcons(
+                                isFavorite: isFavorite,
+                                isInCart: isInCart,
+                              ),
                             ),
-                    );
-                  },
-                );
+                            onTap: () => Navigator.of(context).pushNamed(
+                              productOverviewRoute,
+                              arguments: product,
+                            ),
+                            onDoubleTap: () =>
+                                products.toggleFavoriteStatus(product),
+                            onRightSwipe: () {
+                              if (isInCart) {
+                                removeItem(product);
+                                _showSnackbar(ctx, 'Removido da sacolinha!',
+                                    () => addItem(product));
+                              } else {
+                                addItem(product);
+                                _showSnackbar(
+                                  ctx,
+                                  'Salvo na sacolinha!',
+                                  () => removeItem(product),
+                                );
+                              }
+                            },
+                            rightSwipeBackground: isInCart
+                                ? GestureBackground(
+                                    icon: Icons.shopping_bag,
+                                    label: 'Remover da sacolinha',
+                                    color: const Color(0xFFF5C6BC),
+                                    backgroundColor: const Color(0xFFF2804E),
+                                    alignment: Alignment.centerLeft,
+                                  )
+                                : GestureBackground(
+                                    icon: Icons.shopping_bag,
+                                    label: 'Salvar na sacolinha',
+                                    color: const Color(0xFFF5BCE4),
+                                    backgroundColor:
+                                        Theme.of(context).accentColor,
+                                    alignment: Alignment.centerLeft,
+                                  ),
+                          );
+                        },
+                      );
+                    },
+                  );
+                }
               },
-            ),
+            );
+          }
+        },
+      ),
     );
   }
 }
