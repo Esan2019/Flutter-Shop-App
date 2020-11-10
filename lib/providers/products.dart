e07080b240077badb587a29e4dcc82aa1a1c96f3
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-11-10 16:41:01 -0300

    Add product method successfully integrated with firebase

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -1,5 +1,10 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
+import 'package:flutter/foundation.dart';
+
+import 'package:http/http.dart' as http;
+
+import '../constants.dart';
 import '../models/product.dart';
 
 class Products with ChangeNotifier {
@@ -58,9 +63,25 @@ class Products with ChangeNotifier {
     notifyListeners();
   }
 
-  void addProduct(Product product) {
-    _products.add(product);
-    notifyListeners();
+  Future<void> addProduct(Product product) async {
+    const url = firebaseDatabaseUrl + 'products.json';
+    final productMap = product.toMap();
+
+    try {
+      final response = await http.post(url, body: json.encode(productMap));
+
+      if (response.statusCode != 200)
+        throw 'Não foi possível adicionar o produto.';
+
+      final productId = json.decode(response.body)['name'];
+      productMap['id'] = productId;
+
+      _products.add(Product.fromMap(productMap));
+      notifyListeners();
+      return Future.delayed(Duration(seconds: 8));
+    } on http.ClientException {
+      throw 'Não foi possível estabelecer uma conexão com o servidor. Verifique se você possui conexão com a internet.';
+    }
   }
 
   void deleteProduct(Product product) {
