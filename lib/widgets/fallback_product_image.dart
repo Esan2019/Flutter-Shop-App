7fce4c48437ea5ae10596de89a970dc60d9e5d6e
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-11-12 16:08:24 -0300

    Overlay color in fallback product image widget

--- lib/widgets/fallback_product_image.dart
+++ lib/widgets/fallback_product_image.dart
@@ -6,31 +6,38 @@ class FallbackProductImage extends StatelessWidget {
   final double height, width;
   final Alignment alignment;
   final TextStyle style;
+  final Color overlay;
   const FallbackProductImage({
     @required this.height,
     @required this.width,
     this.alignment,
     this.style,
+    this.overlay,
   })  : assert(height != null),
         assert(width != null);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(16),
       height: height,
       width: width,
-      alignment: alignment ?? Alignment.topCenter,
       decoration: BoxDecoration(
         image: DecorationImage(
           image: AssetImage(fallbackImagePath),
           fit: BoxFit.cover,
         ),
       ),
-      child: Text(
-        'Desculpe, não conseguimos carregar a imagem deste produto',
-        style: style,
-        textAlign: TextAlign.center,
+      child: Container(
+        height: height,
+        width: width,
+        padding: const EdgeInsets.all(16),
+        alignment: alignment ?? Alignment.topCenter,
+        color: overlay,
+        child: Text(
+          'Desculpe, não conseguimos carregar a imagem deste produto',
+          style: style,
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
