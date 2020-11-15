c2b04e8cefbdf62ab1c2c7cb93ab8a165f2c8d5e
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-11-15 03:44:54 -0300

    CartItem fromMap constructor and toMap method

--- lib/models/cart_item.dart
+++ lib/models/cart_item.dart
@@ -4,6 +4,9 @@ class CartItem {
   final Product _product;
   int _quantity;
   CartItem(this._product) : this._quantity = 1;
+  CartItem.fromMap(Map<String, dynamic> map)
+      : this._product = Product.fromMap(map['product']),
+        this._quantity = map['quantity'];
 
   double get totalValue => _product.price * _quantity;
 
@@ -17,5 +20,14 @@ class CartItem {
 
   void decreaseQuantity() {
     if (_quantity > 1) _quantity--;
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'product': product.toMap(),
+      'quantity': quantity,
+      'totalValue': totalValue,
+    };
   }
 }
