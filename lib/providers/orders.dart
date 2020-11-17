3eebb460bb005552a7c3891c619e1f6384d648ea
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-11-17 20:07:48 -0300

    Orders provider successfully integrated with firebase

--- lib/providers/orders.dart
+++ lib/providers/orders.dart
@@ -1,25 +1,59 @@
-import 'dart:math';
+import 'dart:convert';
 
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 import '../models/order.dart';
 import '../models/cart_item.dart';
+import '../constants.dart';
+
+const _ordersUrl = firebaseDatabaseUrl + 'orders.json';
 
 class Orders with ChangeNotifier {
   List<Order> _orders = [];
 
   List<Order> get orders => [..._orders];
 
-  void createOrder(List<CartItem> cartItems) {
-    _orders.insert(
-      0,
-      Order(
-        id: Random().nextInt(2000).toString(),
-        moment: DateTime.now(),
-        items: cartItems,
-      ),
-    );
+  Future<void> fetchOrdersFromDatabase() async {
+    try {
+      final response = await http.get(_ordersUrl);
+      final ordersMap = json.decode(response.body) as Map<String, dynamic>;
 
-    notifyListeners();
+      if (ordersMap == null) throw 'Você ainda não fez nenhum pedido';
+
+      final List<Order> orders = [];
+
+      ordersMap.forEach(
+        (id, orderMap) => orders.insert(
+          0,
+          Order.fromMap(orderMap),
+        ),
+      );
+
+      _orders = orders;
+    } catch (error) {
+      if (error.runtimeType == String) throw error;
+      throw 'Não foi possível carregar os pedidos. Verifique se você possui conexão com a internet.';
+    }
+  }
+
+  Future<void> createOrder(List<CartItem> cartItems) async {
+    final moment = DateTime.now();
+
+    var order = Order(id: null, moment: moment, items: cartItems);
+    final orderMap = order.toMap()..remove('id');
+
+    try {
+      final response = await http.post(_ordersUrl, body: json.encode(orderMap));
+      final decodedResponse = json.decode(response.body);
+
+      orderMap.putIfAbsent('id', () => decodedResponse['name']);
+
+      _orders.insert(0, Order.fromMap(orderMap));
+
+      notifyListeners();
+    } catch (error) {
+      throw 'Não foi possível criar o pedido. Verifique se você possui conexão com a internet.';
+    }
   }
 }
