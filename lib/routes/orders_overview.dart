1f9cb495ef4f215fbb0517d4a7065c66c1eae975
Esan2019 <47592751+Esan2019@users.noreply.github.com>
2020-11-17 20:18:53 -0300

    Fetching orders from database

--- lib/routes/orders_overview.dart
+++ lib/routes/orders_overview.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:provider/provider.dart';
+import 'package:lottie/lottie.dart';
 
 import '../size_config.dart';
 import '../providers/orders.dart';
@@ -10,13 +11,32 @@ class OrdersOverview extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     SizeConfig.init(context);
-    final orders = Provider.of<Orders>(context, listen: false).orders;
+    final ordersProvider = Provider.of<Orders>(context, listen: false);
 
     return Scaffold(
-      body: ListView.builder(
-        itemCount: orders.length,
-        physics: BouncingScrollPhysics(),
-        itemBuilder: (_, index) => ExpandableOrderCard(orders.elementAt(index)),
+      body: FutureBuilder(
+        future: ordersProvider.fetchOrdersFromDatabase(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.error == null) {
+              return ListView.builder(
+                itemCount: ordersProvider.orders.length,
+                physics: BouncingScrollPhysics(),
+                itemBuilder: (_, index) =>
+                    ExpandableOrderCard(ordersProvider.orders.elementAt(index)),
+              );
+            } else {
+              return Center(
+                  child: Text(
+                snapshot.error.toString(),
+                textAlign: TextAlign.center,
+              ));
+            }
+          } else {
+            return Center(
+                child: Lottie.asset('assets/animations/bouncy-balls.json'));
+          }
+        },
       ),
     );
   }
